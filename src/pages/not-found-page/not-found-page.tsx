65f5830c561b5e767cff5796b396c7edbc0e7787
Elena <enafukin@gmail.com>
2024-03-03 18:09:25 +0400

    Добавляет стили для страницы 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,8 @@
 import {Link} from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
 import Footer from '../../components/footer/footer';
+import {AppRoutes} from '../../const';
+import cls from './not-found-page.module.css';
 
 const NotFoundPage = (): JSX.Element => (
   <>
@@ -8,9 +10,9 @@ const NotFoundPage = (): JSX.Element => (
       <title>6 cities. Page not found.</title>
     </Helmet>
     <main className="page__main">
-      <section style={{textAlign: 'center', padding: '15%', borderBottom: '2px solid rgba(222,222,222,.5)'}}>
-        <h1>404. Page not found</h1>
-        <Link to={'/'}>Go back to the main page.</Link>
+      <section className={cls.content}>
+        <h1 className={cls.title}>404. Page not found</h1>
+        <Link className={cls.link} to={AppRoutes.Main}>Go back to the main page.</Link>
       </section>
     </main>
     <Footer />
