e9b1d673e70c54560ab2e0113f333db329d9f873
Elena <enafukin@gmail.com>
2024-03-29 21:58:26 +0400

    Добавляет поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosInstance } from 'axios';
+import axios, {AxiosInstance} from 'axios';
+import {getToken} from './token';
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -9,5 +10,17 @@ export const createAPi = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
   return api;
 };
+
+
