aab29c93fa1177f02a8b869ff30b578965caa3ae
Elena <enafukin@gmail.com>
2024-04-09 12:37:39 +0400

    Дорабатывает отрисовку данных на странице предложения

--- src/components/offer/offer-description/offer-description.tsx
+++ src/components/offer/offer-description/offer-description.tsx
@@ -25,10 +25,10 @@ const OfferDescription = memo(({offer}: OfferDescriptionProps): JSX.Element => {
           {type}
         </li>
         <li className="offer__feature offer__feature--bedrooms">
-          {bedrooms} Bedrooms
+          {bedrooms} Bedroom{bedrooms > 1 && 's'}
         </li>
         <li className="offer__feature offer__feature--adults">
-            Max {maxAdults} adults
+            Max {maxAdults} adult{maxAdults > 1 && 's'}
         </li>
       </ul>
       <div className="offer__price">
